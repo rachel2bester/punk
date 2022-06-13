0de7b963082d2fd51c8ee4176254adae45e3fc70
rachel2bester <rachel2bester@gmail.com>
2022-06-13 16:15:33 +0200

    actually got fetch to work + some beerinfo styling

--- src/containers/BeerInfo/BeerInfo.jsx
+++ src/containers/BeerInfo/BeerInfo.jsx
@@ -3,17 +3,27 @@ import {useParams} from "react-router-dom";
 
 const BeerInfo = () => {
     const {beerId} = useParams();
+    const [loaded, setLoaded] = useState(false);
     const [beer, setBeer] = useState(undefined)
 
     useEffect(() => {
         fetch(`https://api.punkapi.com/v2/beers/${beerId}`)
             .then((response) => response.json())
-            .then((data) => setBeer(data[0]))
+            .then((data) => {
+                setBeer(data[0])
+                setLoaded(true)
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
     }, []);
 
 
     return (
-        <div>Name: {beer.name}</div>
+        <>
+        {loaded && <div>Name: {beer.name}</div>}
+        </>
+        
     )
 }
 
